2e89201284a0527d04107b2de932e033abce4e7e
binnay <125789787+binnay@users.noreply.github.com>
2024-06-18 01:22:12 +0900

    Initial Commit

---
+++ TransitAlert1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TransitAlert1
+//
+//  Created by sungbin on 6/18/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
